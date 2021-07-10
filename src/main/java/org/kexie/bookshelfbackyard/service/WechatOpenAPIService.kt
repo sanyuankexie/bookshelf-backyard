70c249a3c76bbc5c54b7e59c531e4c2077c2035d
VisualDust <Miya@Akasaki.space>
2021-07-10 21:43:17 +0800

    whatever

--- src/main/java/org/kexie/bookshelfbackyard/service/WechatOpenAPIService.kt
+++ src/main/java/org/kexie/bookshelfbackyard/service/WechatOpenAPIService.kt
@@ -40,8 +40,8 @@ class WechatOpenAPIService {
     val codeVerified = mutableMapOf<String, String>()
 
     companion object {
-        val appid = "wxb53ed26e17dd8e42"
-        val appsecret = "8c0c5d14e8b3f7dc33c2e84467de48db"
+        val appid = "wx718982cd8b3edaa7"
+        val appsecret = "b1cb5c7c65ae67ac55981a0fe13561a1"
         val grantType="authorization_code"
     }
 
